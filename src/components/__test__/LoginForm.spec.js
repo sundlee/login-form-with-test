fc6df4aecf9236740335e759c0b1f1267d361e98
Yong-Hae Lee <sundlee@gmail.com>
2020-05-26 15:50:32 +0900

    @vue/test-utils의 shallowMount로 인스턴스 생성시 테스트를 위한 데이터를 주입

--- src/components/__test__/LoginForm.spec.js
+++ src/components/__test__/LoginForm.spec.js
@@ -8,8 +8,14 @@ describe('LoginForm.vue', () => {
   });
 
   test('ID는 이메일 형식이어야 한다.', () => {
-    const wrapper = shallowMount(LoginForm);
+    const wrapper = shallowMount(LoginForm, {
+      data() {
+        return {
+          username: 'test',
+        };
+      },
+    });
     const idInput = wrapper.find('#username');
-    console.log(idInput.html());
+    console.log(idInput.element.value);
   });
 });
