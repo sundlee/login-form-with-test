c4928e5771f8d632b4a8827b94a248f85d8b2fb5
Yong-Hae Lee <sundlee@gmail.com>
2020-05-26 11:14:33 +0900

    vue-cli 생성 > 로그인 페이지 예제 추가

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-https://github.com/sundlee/login-form-with-test.git
 # login-form-with-test
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Run your unit tests
+```
+yarn test:unit
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="contents">
+    <div class="form-wrapper form-wrapper-sm">
+      <form @submit.prevent="submitForm" class="form">
+        <div>
+          <label for="username">id:</label>
+          <input id="username" type="text" v-model="username" />
+          <p class="validation-text">
+            <span class="warning" v-if="!isUsernameValid && username">
+              Please enter an email address
+            </span>
+          </p>
+        </div>
+        <div>
+          <label for="password">pw:</label>
+          <input id="password" type="text" v-model="password" />
+        </div>
+        <button
+          :disabled="!isUsernameValid || !password"
+          type="submit"
+          class="btn"
+          :class="!isUsernameValid || !password ? 'disabled' : null"
+        >
+          로그인
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { validateEmail } from '@/utils/validation';
+
+export default {
+  data() {
+    return {
+      // form values
+      username: '',
+      password: '',
+    };
+  },
+  computed: {
+    isUsernameValid() {
+      return validateEmail(this.username);
+    },
+  },
+  methods: {
+    submitForm() {
+    },
+  },
+};
+</script>
+
+<style>
+.btn {
+  color: white;
+}
+</style>
