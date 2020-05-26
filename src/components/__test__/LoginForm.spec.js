e8d18f928f3d74d7cb93aa19a279a418c684fe0a
Yong-Hae Lee <sundlee@gmail.com>
2020-05-26 15:38:01 +0900

    @vue/test-utils의 shallowMount으로 변경

--- src/components/__test__/LoginForm.spec.js
+++ src/components/__test__/LoginForm.spec.js
@@ -1,10 +1,9 @@
-import Vue from 'vue';
+import { shallowMount } from '@vue/test-utils';
 import LoginForm from '../LoginForm.vue';
 
 describe('LoginForm.vue', () => {
   test('컴포넌트가 마운팅되면 username이 존재하고 초기 값으로 설정 되야 한다.', () => {
-    const instance = new Vue(LoginForm).$mount();
-    console.log(instance.username);
-    expect(instance.username).toBe('');
+    const wrapper = shallowMount(LoginForm);
+    expect(wrapper.vm.username).toBe('');
   });
 });
