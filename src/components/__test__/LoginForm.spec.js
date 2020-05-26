7f15ca2a28e5dfed90e8c11960314ef828ea9541
Yong-Hae Lee <sundlee@gmail.com>
2020-05-26 15:29:04 +0900

    컴포넌트 생성/ 기본값 확인 테스트 추가 (설명 보강)

--- src/components/__test__/LoginForm.spec.js
+++ src/components/__test__/LoginForm.spec.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import LoginForm from '../LoginForm.vue';
 
 describe('LoginForm.vue', () => {
-  test('컴포넌트가 마운팅되면 화면에 그려져야 한다.', () => {
+  test('컴포넌트가 마운팅되면 username이 존재하고 초기 값으로 설정 되야 한다.', () => {
     const instance = new Vue(LoginForm).$mount();
     console.log(instance.username);
     expect(instance.username).toBe('');
